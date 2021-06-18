45d08e5066ec9557b9691bf5eabf355956bbd43f
Maxvyr <maxvy_@naver.com>
2021-06-18 11:07:00 +0200

    widget test add

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:adopt_ta_waifu/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('show Loading on Home Page', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    var loading = find.byType(CircularProgressIndicator);
+    expect(loading, findsOneWidget);
   });
 }
